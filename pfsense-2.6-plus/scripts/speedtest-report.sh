9c00c90594c176d2a123d251d9dcfff7d2a5d591
Andrey Gheno Piekas <andrey.piekas@gmail.com>
2025-06-01 22:53:43 -0300

    Update speedtest-report.sh

--- pfsense-2.6-plus/scripts/speedtest-report.sh
+++ pfsense-2.6-plus/scripts/speedtest-report.sh
@@ -3,14 +3,14 @@
 DIRECTORYREPORT="/usr/local/www/speedtest"
 INTERFACE01="vtnet1"
 
-SOURCE01=$(/sbin/ifconfig $INTERFACE01 | /usr/bin/grep -w inet | /usr/bin/sed -e 's/^[[:space:]]*//' | /usr/bin/cut -d ' ' -f2)
-DESCRIPTION01=$(/sbin/ifconfig $INTERFACE01 | /usr/bin/grep -w description | /usr/bin/sed -e 's/^[[:space:]]*//' | /usr/bin/cut -d ' ' -f2)
+SOURCE01=$(/sbin/ifconfig $INTERFACE01 | grep -w inet | sed -e 's/^[[:space:]]*//' | cut -d ' ' -f2)
+DESCRIPTION01=$(/sbin/ifconfig $INTERFACE01 | grep -w description | sed -e 's/^[[:space:]]*//' | cut -d ' ' -f2)
 PAGEREPORT01="$INTERFACE01.html"
 
 if [ -d "$DIRECTORYREPORT" ]; then
   echo "Diretório: $DIRECTORYREPORT - OK"
 else
-  /bin/mkdir $DIRECTORYREPORT
+  /bin/mkdir "$DIRECTORYREPORT"
   echo "Diretório: $DIRECTORYREPORT criado com sucesso!!!"
 fi
 
